04680d83c06118ca6d9b4510aaa9b8a346d462b5
storm37000 <6331913+storm37000@users.noreply.github.com>
2024-11-13 11:33:42 -0500

    hopefully fixes #1

--- lua/autorun/simple_e2_theft_protection.lua
+++ lua/autorun/simple_e2_theft_protection.lua
@@ -22,67 +22,51 @@ else
 		duplicator = true,
 		advdupe2 = true,
 		adv_duplicator = true,
+		wire_expression2 = true
 	}
-	
-	local function duplicationCheck( ply, trace )
-		local ret = {}
-		local entities = constraint.GetAllConstrainedEntities( trace.Entity )
-		entities[trace.Entity] = trace.Entity -- add the entity itself, too (it might be an E2)
-		
-		for k,v in pairs( entities ) do
-			if v:GetClass() == "gmod_wire_expression2" then
-				ret[#ret+1] = v
-			end
-		end
-		
-		return ret
-	end
 	
 	local function check( ply, trace, toolname, other )
 		if not IsValid( trace.Entity ) then return end
 		
-		if toolname == "wire_expression2" and other == "halt execution" then -- just halting execution is fine, don't block that at all
-			return
-		end
-		
 		if duplicators[toolname] then
-			local entities = duplicationCheck( ply, trace )
+			if toolname == "wire_expression2" and other == "halt execution" then -- just halting execution is fine, don't block that at all
+				return
+			end
+
+			local entities = constraint.GetAllConstrainedEntities( trace.Entity )
 			
 			-- Here we need to check all E2s on the contraption.
 			-- because they might be owned by different people
 			-- If you're not allowed to dupe ANY of the E2s on this contraption, prevent tool click
 			for i=1,#entities do
-				if not check( ply, { Entity = entities[i] }, "wire_expression2_theft_protection" ) then
-					return false
-				end
-			end
-		end
-		
-		if trace.Entity:GetClass() == "gmod_wire_expression2" then
-			local owner = trace.Entity:CPPIGetOwner()
-			
-			local mode = owner:GetInfoNum( "wire_expression2_theft_protection", 2 )
-			if mode == 0 then return end
-		
-			if mode == 1 then -- prop protection friends list can take your code
-				local friends = owner:CPPIGetFriends()
+				local entity = entities[i]
+				if entity:GetClass() == "gmod_wire_expression2" then
+					local owner = entity:CPPIGetOwner()
+					
+					local mode = owner:GetInfoNum( "wire_expression2_theft_protection", 2 )
+					if mode == 0 then return end
 				
-				local found = false
-				for i=1,#friends do
-					if friends[i] == ply then
-						found = true
-						break
+					if mode == 1 then -- prop protection friends list can take your code
+						local friends = owner:CPPIGetFriends()
+						
+						local found = false
+						for i=1,#friends do
+							if friends[i] == ply then
+								found = true
+								break
+							end
+						end
+						
+						if found == false then
+							return false
+						end
+						
+					elseif mode == 2 and owner ~= ply and not ply:IsSuperAdmin() then -- only super admins can take your code
+						return false
+					elseif mode == 3 and owner ~= ply then -- Nobody can take your code
+						return false
 					end
 				end
-				
-				if found == false then
-					return false
-				end
-				
-			elseif mode == 2 and owner ~= ply and not ply:IsSuperAdmin() then -- only super admins can take your code
-				return false
-			elseif mode == 3 and owner ~= ply then -- Nobody can take your code
-				return false
 			end
 		end
 	end
